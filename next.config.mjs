59449f5f2e749bf98294a76f03af79fb94fff599
Mohosin999 <mohosin.hasan.akash@gmail.com>
2025-02-20 10:22:02 +0600

    updated next.config.mjs file

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  eslint: {
+    // Ignore ESLint errors during builds
+    ignoreDuringBuilds: true,
+  },
+};
 
 export default nextConfig;
