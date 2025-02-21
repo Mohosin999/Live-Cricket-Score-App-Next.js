57d659a81d0c0addacee5b7bb6fe8115f8c6af42
Mohosin999 <mohosin.hasan.akash@gmail.com>
2025-02-21 14:51:42 +0600

    recent and schedule matches page again included

---
+++ src/app/recent-matches/domestic/page.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Wrapper from "@/components/Wrapper";
+import MatchOverview from "@/components/MatchOverview";
+import { getRecentMatches } from "@/lib/cricketApi";
+
+const DomesticRecentMatches = async () => {
+  // Get all recent matches
+  const recentMatches = await getRecentMatches();
+
+  // Filter matches related to domestic
+  const filteredRecent = recentMatches.typeMatches.filter(
+    (matches) => matches.matchType === "Domestic"
+  );
+
+  // console.log("console from recent domestic -> ", recentMatches);
+
+  return (
+    <Wrapper>
+      <div className="pt-6">
+        <MatchOverview
+          matches={filteredRecent}
+          path={"/recent-matches/domestic"}
+        />
+      </div>
+    </Wrapper>
+  );
+};
+
+export default DomesticRecentMatches;
---
+++ src/app/recent-matches/international/page.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Wrapper from "@/components/Wrapper";
+import MatchOverview from "@/components/MatchOverview";
+import { getRecentMatches } from "@/lib/cricketApi";
+
+const InternationalRecentMatches = async () => {
+  // Get all recent matches
+  const recentMatches = await getRecentMatches();
+
+  // Filter matches related to international
+  const filteredRecent = recentMatches.typeMatches.filter(
+    (matches) => matches.matchType === "International"
+  );
+
+  return (
+    <Wrapper>
+      <div className="pt-6">
+        <MatchOverview
+          matches={filteredRecent}
+          path={"/recent-matches/international"}
+        />
+      </div>
+    </Wrapper>
+  );
+};
+
+export default InternationalRecentMatches;
