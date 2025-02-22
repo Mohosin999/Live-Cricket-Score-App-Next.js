bacc0021cf1ff97a1e9d2e9c7789d083d179ecfe
Mohosin999 <mohosin.hasan.akash@gmail.com>
2025-02-22 11:11:48 +0600

    refactoring

--- src/app/page.js
+++ src/app/page.js
@@ -1,90 +1,37 @@
-// import Wrapper from "@/components/Wrapper";
-// import UpcomingSeries from "@/components/UpcomingSeries";
-// import {
-//   getInternationalNews,
-//   getLiveMatches,
-//   getSeries,
-// } from "@/lib/cricketApi";
-// import LatestNews from "@/components/LatestNews";
-// import MatchOverview from "@/components/MatchOverview";
-
-// const Home = async () => {
-//   const liveMatches = await getLiveMatches();
-//   // Get matches array from live matches object
-//   const filteredLiveMatches = liveMatches?.typeMatches;
-
-//   const series = await getSeries();
-//   const internationNews = await getInternationalNews();
-
-//   return (
-//     <Wrapper>
-//       <div className="grid grid-cols-6 gap-10 pt-6">
-//         {/* Live Matches Section */}
-//         <div className="col-span-2">
-//           <div className="space-y-4 h-screen overflow-y-auto no-scrollbar">
-//             <MatchOverview matches={filteredLiveMatches} colSpan1={true} />
-//           </div>
-//         </div>
-
-//         {/* Latest News Section */}
-//         <div className="col-span-3">
-//           <div className="py-4 h-screen overflow-y-auto no-scrollbar">
-//             <LatestNews newsObj={internationNews} />
-//           </div>
-//         </div>
-
-//         {/* Series and Upcoming series */}
-//         <div className="col-span-1">
-//           <div className="space-y-4 h-screen overflow-y-auto no-scrollbar">
-//             <UpcomingSeries series={series} />
-//           </div>
-//         </div>
-//       </div>
-//     </Wrapper>
-//   );
-// };
-
-// export default Home;
-
 import Wrapper from "@/components/Wrapper";
-import UpcomingSeries from "@/components/UpcomingSeries";
 import {
-  getInternationalNews,
   getLiveMatches,
-  getSeries,
+  getUpcomingInterantionalMatches,
 } from "@/lib/cricketApi";
-import LatestNews from "@/components/LatestNews";
 import MatchOverview from "@/components/MatchOverview";
+import UpcomingMatches from "@/components/UpcomingMatches";
 
 const Home = async () => {
   const liveMatches = await getLiveMatches();
-  // Get matches array from live matches object
-  const filteredLiveMatches = liveMatches?.typeMatches;
+  const upcomingMatches = await getUpcomingInterantionalMatches();
 
-  const series = await getSeries();
-  const internationNews = await getInternationalNews();
+  // Get live match array from live matches object
+  const liveMatchesArr = liveMatches?.typeMatches;
+
+  // Filter upcoming matches from upcoming matches object
+  const filteredIUpcomingMatches = upcomingMatches?.matchScheduleMap?.filter(
+    (day) => day.scheduleAdWrapper
+  );
 
   return (
     <Wrapper>
-      <div className="grid grid-cols-6 gap-10 pt-2">
+      <div className="grid grid-cols-5 gap-10">
         {/* Live Matches Section */}
         <div className="col-span-2">
           <div className="space-y-4">
-            <MatchOverview matches={filteredLiveMatches} colSpan1={true} />
-          </div>
-        </div>
-
-        {/* Latest News Section */}
-        <div className="col-span-3">
-          <div className="py-4">
-            <LatestNews newsObj={internationNews} />
+            <MatchOverview matches={liveMatchesArr} colSpan1={true} />
           </div>
         </div>
 
         {/* Series and Upcoming series */}
-        <div className="col-span-1">
+        <div className="col-span-3">
           <div className="space-y-4">
-            <UpcomingSeries series={series} />
+            <UpcomingMatches matches={filteredIUpcomingMatches} />
           </div>
         </div>
       </div>
